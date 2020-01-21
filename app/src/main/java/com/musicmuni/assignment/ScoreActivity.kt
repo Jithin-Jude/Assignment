a7156d3076363175887ca0a565d0cffa82735386
Jithin Jude <jithin.jude68@gmail.com>
2020-01-21 18:16:56 +0530

    grid lines done.

--- app/src/main/java/com/musicmuni/assignment/ScoreActivity.kt
+++ app/src/main/java/com/musicmuni/assignment/ScoreActivity.kt
@@ -2,11 +2,17 @@ package com.musicmuni.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ScoreActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val myView = GridView(this)
+
+        graph_background.addView(myView)
     }
 }
