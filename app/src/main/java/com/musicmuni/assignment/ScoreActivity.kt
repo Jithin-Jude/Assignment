a35208120e3dcdd9104cc7294e015e516d4325fb
Jithin Jude <jithin.jude68@gmail.com>
2020-01-22 17:19:25 +0530

    converted to property access syntax_3.

--- app/src/main/java/com/musicmuni/assignment/ScoreActivity.kt
+++ app/src/main/java/com/musicmuni/assignment/ScoreActivity.kt
@@ -18,7 +18,7 @@ class ScoreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main)
 
