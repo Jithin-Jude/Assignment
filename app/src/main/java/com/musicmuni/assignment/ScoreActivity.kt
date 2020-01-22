27d3586de617b8a65bfe4c853b0bf945d7c894d2
Jithin Jude <jithin.jude68@gmail.com>
2020-01-22 10:37:31 +0530

    statusbar hide.

--- app/src/main/java/com/musicmuni/assignment/ScoreActivity.kt
+++ app/src/main/java/com/musicmuni/assignment/ScoreActivity.kt
@@ -2,14 +2,17 @@ package com.musicmuni.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import kotlin.collections.ArrayList
 
 class ScoreActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main)
 
